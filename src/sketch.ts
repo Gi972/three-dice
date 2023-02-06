4efad746bb8e6c4ed125810bf520340d3b46b6c9
Gi972 <jeanchristophedemar@yahoo.fr>
2023-02-06 22:02:01 +0100

    final raw dice

--- src/sketch.ts
+++ src/sketch.ts
@@ -4,6 +4,9 @@ import GUI from 'lil-gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { Color } from 'three';
 
+//gray = 0xb0b0b0
+//const color_black = 'black';
+
 const gui = new GUI();
 
 const size = {
@@ -11,17 +14,16 @@ const size = {
   height: 400,
 };
 
+//*SCENE
 let canvas: HTMLCanvasElement = document.querySelector('.webgl')!;
-const scene = new THREE.Scene();
-scene.background = new Color(0xb0b0b0);
 
 const box = new THREE.BoxGeometry(1, 1, 1);
 const circle = new THREE.SphereGeometry(0.05, 32);
 
-const material = new THREE.MeshBasicMaterial({ color: 'white' });
-const pointMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xe71837 });
+const pointMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
 
-const container = new THREE.Mesh(box, material);
+const container = new THREE.Mesh(box, boxMaterial);
 const point1 = new THREE.Mesh(circle, pointMaterial);
 point1.position.z = 0.5;
 
@@ -88,39 +90,59 @@ const point6 = new THREE.Group();
 point6.add(point6_1, point6_2, point6_3, point6_4, point6_5, point6_6);
 point6.position.z = -0.5;
 
-const planeGeometry = new THREE.PlaneGeometry(4, 4);
-const groundMaterial = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
+const planeGeometry = new THREE.PlaneGeometry(5, 3);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0x35654d,
   side: THREE.DoubleSide,
 });
 const ground = new THREE.Mesh(planeGeometry, groundMaterial);
 ground.rotation.x = Math.PI * 0.5;
 
-const debugUi = new THREE.Group();
-const axesHelper = new THREE.AxesHelper(2);
-const helper = new THREE.GridHelper(6, 6);
-debugUi.add(helper, axesHelper);
-
 const dice = new THREE.Group();
-dice.position.y = 0.55;
+dice.position.y = 0.11;
+dice.scale.set(0.2, 0.2, 0.2);
 dice.add(container, point1, point2, point3, point4, point5, point6);
 
-scene.add(dice, debugUi, ground);
+const dice2 = dice.clone();
+dice2.position.x = 0.25;
+dice2.rotation.y = 0.4;
 
 gui.add(point2.position, 'x').min(-3).max(3).step(0.1);
 gui.add(point2.rotation, 'y').min(-3).max(3).step(0.1);
 
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 4;
-scene.add(camera);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const pointLight = new THREE.PointLight(0xff9000, 0.5);
+pointLight.position.set(0, 0.2, 0.2);
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 1;
+
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setSize(size.width, size.height);
-//renderer.render(scene, camera);
+
+//* DEBUG
+const debugUi = new THREE.Group();
+const axesHelper = new THREE.AxesHelper(6);
+const helper = new THREE.GridHelper(6, 6);
+const cameraHelper = new THREE.CameraHelper(camera);
+
+debugUi.add(helper, axesHelper);
+debugUi.position.y = -0;
+
+const scene = new THREE.Scene();
+//scene.add(debugUi)
+
+scene.add(camera, dice, dice2, ground);
+scene.add(ambientLight);
+scene.add(pointLight);
 
 const clock = new THREE.Clock();
 const controls = new OrbitControls(camera, renderer.domElement);
 
+//renderer.render(scene, camera);
 function gameLoop() {
   //dice.rotation.y = clock.getElapsedTime();
   //point2.position.x = Math.cos(clock.getElapsedTime());
